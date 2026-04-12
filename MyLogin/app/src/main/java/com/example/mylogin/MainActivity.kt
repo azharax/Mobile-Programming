9e2b436744aba89b92ef801b28a70fa7ad60b94d
Azhar Aziz <muhammmadazharaziz@gmail.com>
2026-04-12 21:01:05 +0700

    Upload MyLogin Pertemuan 7

---
+++ MyLogin/app/src/main/java/com/example/mylogin/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.example.mylogin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mylogin.ui.theme.MyLoginTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyLoginTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.a),
+            contentDescription = "Login image",
+            modifier = Modifier.size(200.dp)
+        )
+
+        Text(text = "Welcome Back", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = "Login to your account")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Email address") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Password") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {  }) {
+            Text(text = "Login")
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = "Forgot Password?",
+            modifier = Modifier.clickable {  }
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(text = "Or sign in with")
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(40.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.fb),
+                contentDescription = "Facebook",
+                modifier = Modifier.size(60.dp).clickable { }
+            )
+            Image(
+                painter = painterResource(id = R.drawable.google),
+                contentDescription = "Google",
+                modifier = Modifier.size(60.dp).clickable { }
+            )
+            Image(
+                painter = painterResource(id = R.drawable.twitter),
+                contentDescription = "Twitter",
+                modifier = Modifier.size(60.dp).clickable { }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    MyLoginTheme {
+        LoginScreen()
+    }
+}
